af84f654bf451d1148e0cc461b17eef9f1367e0c
tarajaneseton <tarajaneseton@gmail.com>
2023-01-02 22:38:48 +0000

    still no progress

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import "./styles/App.css";
-import {Productlist, Product} from "./components/Product";
+import React, { useState } from "react";
+import { Track }  from "./components/Product";
 import data from "./models/data.json";
+import Help from "./components/Product";
 
-function App() {
-  const [products] = useState(data);
-  return ( <ul>
-    {products.map((product)=><Product product={product}/>)}
-  </ul>
-// // {/* <div className="App">
-//       <header className="App-header"> */}
-        // <Productlist products= {Product}/>;
-//         // <p>
-//         //   Edit <code>src/App.js</code> and save to reload.
-//         // </p>
-//         // <a
-//         //   className="App-link"
-//         //   href="https://imusicstore.netlify.app/"
-//         //   target="_blank"
-//         //   rel="noopener noreferrer"
-//         // >
-//         //  View Example Mediastore App
-//         // </a>
-//     //   </header>
-//     // </div>
+export default function App() {
+  return (
+    <div className="App">
+      <Car />
+    </div>
   );
 }
+//usestate is used to store the tracks in the state.
+// const App = (props) => {
+// const [tracks] = useState(data);
+// return tracks.map(track => <Track key={track.id} track={track}/>);
+// // }
+// export default App;
 
-export default App;
+
+<div className="App">
+      <header className="App-header">
+      <Productlist products= {Product}/>;
+ <p>Edit <code>src/App.js</code> and save to reload.
+        </p>
+<a className="App-link"
+href="https://imusicstore.netlify.app/"
+ target="_blank"
+rel="noopener noreferrer"
+        >
+         View Example Mediastore App
+        </a>
+      </header>
+</div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,69 @@
-import React from 'react';
-import data from "./models/data.json";
+import React, {useState} from 'react';
+// import data from "./models/data.json";
+import PropTypes from "prop-types";
 
-export function Product(props) {
-    // const books = data;
-    // const{ trackName, artistName, trackPrice, artworkUrl100 } = props
-    return (
-        <div>
-            <h1>{props.trackName}</h1>
-            <h2>{props.artistName}</h2>
-            <h3>{props.trackPrice}</h3>
-            <img src={props.artworkUrl100} alt={trackName}/>
-        </div>
-    );
+    
+
+// slides
+// const { artistName,
+//         trackName,
+//         trackPrice,
+//         currency,
+//         artworkUrl60
+//  } = props.track;
+
+//  <div className="track">
+//     <img src={artworkUrl60} alt={title}/>
+//     <div>
+//         <h2 artistName={artistName}>{artistName}</h2>
+//         <h2 trackName={trackName}>{trackName}</h2>
+//     </div>
+//  </div>
+export function Track(props) {
+return (
+    <div>
+    <h3>{trackName}</h3>
+    <h3>{artistName}</h3>
+    <h3>{trackPrice}</h3>
+    </div>
+);
 }
 
-export const Productlist = (props) => 
-{
-    return (
-        <main>
-            {props.products.map(
-                (item) => 
-                <Product trackName={item.trackName} artistName={item.artistName} trackPrice={item.trackPrice} artworkUrl100={item.artworkUrl100}/>
-                )}
-        </main>
-        )
-    }
+// * <div className="track">
+//     <img src={artworkUrl100} alt={trackName}/>
+// <div>
+//     <h2 trackName={trackName}>{trackName}</h2>
+//     <h2 artistName={artistName}>{artistName}</h2>
+//     <h2 trackPrice={trackPrice}>{trackPrice}</h2>
+
+// </div>
+// </div>
+// ); */}
+// // export function Product(props) {
+//     // const books = data;
+//     // const { trackName, artistName, trackPrice, artworkUrl100 } = props
+
+// // export const Track = (props) => {
+// //     return (
+// //             <div>
+// //             <h1>{props.trackName}</h1>
+// //             <h2>{props.artistName}</h2>
+// //             <h3>{props.trackPrice}</h3>
+// //             <img src={props.artworkUrl100} alt={props.trackName}/>
+       
+// //         </div>
+// //     );
+        
+// // }
+
+// // export const Productlist = (props) => 
+// // {
+// //     return (
+// //         <main>
+// //             {props.products.map(
+// //                 (item) => 
+// //                 <Product trackName={item.trackName} artistName={item.artistName} trackPrice={item.trackPrice} artworkUrl100={item.artworkUrl100}/>
+// //                 )}
+// //         </main>
+// //         )
+// //     }
