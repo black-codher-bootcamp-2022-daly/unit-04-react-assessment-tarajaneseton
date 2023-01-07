2b000a7e1ba30177073c367673fb65dd086db342
tarajaneseton <tarajaneseton@gmail.com>
2023-01-07 13:16:01 +0000

    fixed breaking issues relating to ProductList deconstructed props correctly

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,81 +1,59 @@
-import React from 'react';
+import React from "react";
 // import "./styles/App.css"
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
-//Each object in the JSON file should be rendered by your <Product/> component.
+const Product = (props) => {
+  const {
+    trackName,
+    artistName,
+    trackPrice,
+    kind,
+    trackId,
+    thumbnail,
+    addToBasket,
+    removeFromBasket,
+  } = props;
 
-// Each product should have a button to Add to Basket. When clicked the item should be removed from <ProductList/> and added to your <Basket/> component.
-// Expected props for the <Product/> component are listed below:
-
-// Props
-// Example expected props. Please note this is not an exhaustive list, but the minimum props required for assessment.
-
-// <Product
-//     kind={kind}
-//     id={trackId}
-//     name={trackName}
-//     thumbnail={artworkUrl100}
-//     price={trackPrice}
-//     addToBasket={addToBasket}
-//     removeFromBasket={removeFromBasket} />
-
-//here i am copying tobias code. it still doesnt work
-
-// function Product({product}) {
-//     const { name } = product.trackName;
-//     const { id } = product.trackId;
-//     const { kind } = product.kind;
-//     return (
-//         <div>
-//             <h2>{name}</h2>
-//         </div>
-//     );
-// }
-
-// export default Product;
-
-// The below code creates props. its been created using the slides video but doesnt work
-
-function Product({ trackName, artistName, trackPrice, kind, trackId,  }) {
-    return (
-        <div style={{
-            backgroundColor: "lilac",
-            padding: "10px",
-            marginBottom: "4px"
-        }}
-        >
-            <h2>{trackName}</h2>
-            <h2>{artistName}</h2>
-            <h3>£{trackPrice}</h3>
-
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        backgroundColor: "purple",
+        padding: "10px",
+        marginBottom: "4px",
+      }}
+    >
+      <img src={thumbnail} alt={trackName} />
+      <h2>{trackName}</h2>
+      <h2>{artistName}</h2>
+      <h3>£{trackPrice}</h3>
+    </div>
+  );
+};
 
 export default Product;
 
-Product.propTypes = {
-    trackName: PropTypes.string.isRequired,
-    artistName: PropTypes.string.isRequired,
-    trackPrice: PropTypes.number.isRequired
+// Product.propTypes = {
+//     trackName: PropTypes.string.isRequired,
+//     artistName: PropTypes.string.isRequired,
+//     trackPrice: PropTypes.number.isRequired
 
-}
- 
-    // const { kind,
-    //     trackId,
-    //     trackName,
-    //     artworkUrl100,
-    //     trackPrice
-    //     addToBasket
-    //     removeFromBasket } = props.music;
-   
-    // <div className="music">
-    // kind={kind}
-    // id={trackId}
-    // <h2 name={trackName}>{trackName}</h2>
-    // <img src thumbnail={artworkUrl100}/>
-    // price={trackPrice}
-    // addToBasket={addToBasket}
-    // removeFromBasket={removeFromBasket} />
-    // </div>
+// }
+
+// const { kind,
+//     trackId,
+//     trackName,
+//     artworkUrl100,
+//     trackPrice
+//     addToBasket
+//     removeFromBasket } = props.music;
+
+// <div className="music">
+// kind={kind}
+// id={trackId}
+// <h2 name={trackName}>{trackName}</h2>
+// <img src thumbnail={artworkUrl100}/>
+// price={trackPrice}
+// addToBasket={addToBasket}
+// removeFromBasket={removeFromBasket} />
+// </div>
 // }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,3 +11,39 @@
 //     items={items}
 //     addToBasket={addToBasket} />
 
+import Product from './Product';
+import React from "react";
+
+
+const ProductList = ( props ) => {
+const {products}=props
+console.log("products",products);
+
+    return (
+        <div>
+            <h1 className="title">Suggested For You</h1>{" "}
+            {(!products || products.length === 0) ? (
+                <div className="empty">No items found...</div>
+            ) : (
+          products.map((product) => (
+            <div className="product" key={product.trackId}>
+          <Product 
+          item={product}
+          id={product.trackId}
+          key={product.trackId} 
+          name={product.trackName} 
+          {...product}
+          thumbnail={product.artworkUrl100}
+          artistName={product.artistName} 
+          price={product.trackPrice}
+          addToBasket={product.addToBasket}
+          removeFromBasket={product.removeFromBasket}
+          />
+          </div>
+          ))
+        )}
+         </div> 
+          );
+};
+
+export default ProductList;
