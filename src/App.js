59565ac50408d983382f12b48dfda465d038444c
tarajaneseton <tarajaneseton@gmail.com>
2023-01-14 13:26:49 +0000

    api and search working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import "./styles/App.css";
+import "./styles/App.css";
 import React, { useState } from "react";
 import data from "./models/data.json";
 import ProductList from "./components/ProductList";
@@ -6,16 +6,47 @@ import { Search } from "./components/Search";
 import About from "./pages/About";
 import Basket from "./components/Basket";
 import Header from "./components/Header";
-// tobia's way: import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 function addBook(id) {
   console.log(`The product ${id} was clicked`);
 }
-// const [products, setProducts]
+
 export function App() {
-  const [products] = useState(data);
-  const [term, setTerm, search] = useState(" ");
+  const [products, setProducts] = useState(data);
+  // const [term, setTerm, search] = useState(" ");
+  const [term, setTerm] = useState(" ");
+
+  //Your app should display the results of the search in an component called <ProductList/> with the identifier id="results" e.g. <div id="results"/>.
+
+
+  async function search(value) {
+    console.log("find books that got clicked", value);
+    const url = `https://itunes.apple.com/search?term=${value}&limit=30&explicit=no`;
+
+    const results = await fetch(url).then((res) => res.json());
+    if (!results.error) {
+      setProducts(
+        results.results.filter(
+          (result) =>
+          result.trackName
+        )
+      )
+      // setProducts(results.products);
+    }
+  }
+
+  function Home() {
+    return (
+      <>
+        <Header />
+        <Search term={term} setTerm={setTerm} handleSubmit={search} />
+        {/* <ProductList /> */}
+        <ProductList products={products} />
+        {/* <ProductList products={products} addToBasket={addToBasket} removeFromBasket={removeFromBasket} productCount={products.length} /> */}
+      </>
+    );
+  }
 
   return (
     <div className="wrapper">
@@ -41,24 +72,12 @@ export function App() {
       </BrowserRouter>
     </div>
   );
-
-  function Home() {
-    return (
-      <>
-        <Header />
-        <Search term={term} setTerm={setTerm} search={search} />
-        {/* <ProductList /> */}
-        <ProductList products={products} productCount={products.length} />
-        {/* <ProductList products={products} addToBasket={addToBasket} removeFromBasket={removeFromBasket} productCount={products.length} /> */}
-      </>
-    );
-  }
 }
 
 // might need to change Basket to Basketlist
 
 //     setProducts(results.items); after   if (!results.error) {
-
+//asynchronus function waits for fetch to request data from a different url
 // async function search(value) {
 //   const url = `https://itunes.apple.com/search?term=${value}&limit=30&explicit=no`;
 //   const results = await fetch(url).then((res) => res.json());
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,72 +2,32 @@ import React from "react";
 // import PropTypes from "prop-types";
 
 export const Search = (props) => {
-  const { search, term, setTerm } = props;
+  const { term, setTerm, handleSubmit } = props;
   // const [value, setValue] = React.useState(" ")
 
   function handleChange(changeEvent) {
-    console.log("tara event =>", changeEvent);
     // props.search(props.term);
     setTerm(changeEvent.target.value);
   }
 
+function onSubmit(event){
+  event.preventDefault()
+          handleSubmit(term)
+}
+
   return (
     <form id="searchAPI">
-   <p style={{color:"violet"}}><em>{term && 'Keywords Typed: ' + term}</em></p> 
+      <p style={{ color: "violet" }}>
+        <em>{term && "Keywords Typed: " + term}</em>
+      </p>
+      <input type="text" value={props.term} onChange={handleChange} id="term" />
       <input
-        type="text"
-        value={props.term}
-        onChange={handleChange}
-        id="term"
+        type="submit"
+        onClick={onSubmit}
       />
-      <input type="submit" />
     </form>
   );
 };
-
-// 11.01.23 search component. search bar displays using value, set valuee instead of props.keyword but it doesnt search for books. updated code sets the value from props video slides 00:58
-
-// this seemed to work, so revret back to this if neeeded. 48mins into video
-// const [keyword, setKeyword] = React.useState("");
-//   return (
-//     <form id="searchAPI">
-//     <div>
-//       <input type="text" value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)}/>
-//       <input type="submit"/>
-//     </div>
-//     </form>
-//   );
-// };
-
-// Search.proptypes = {
-//   keyword: PropTypes.string,
-//   setKeyword: PropTypes.func
-// }
-
-//   const { search, term, setTerm } = props;
-
-//   function handleChange(changeEvent) {
-//     console.log("Someone Typed event =>", changeEvent);
-//      props.search(props.term);
-//     setTerm(changeEvent.target.value);
-//   }
-
-//   //    handleSubmit(term); 3rd line
-
-//   function onSubmit(event) {
-//     event.preventDefault();
-
-//   }
-//   return (
-//     <form className="Search" id="searchAPI">
-//       <p style={{ color: "pink" }}>
-//         <em>{term && "Term Typed: " + term}</em>
-//       </p>
-//       <input id="term" type="text" value={props.term} onChange={handleChange} />
-//       <input className="submitButton" type="submit" onClick={onSubmit} />
-//     </form>
-//   );
-// }
 
 // //Search.js will return a form with the following
 // identifier id="searchAPI" e.g., <form id="searchAPI">.
